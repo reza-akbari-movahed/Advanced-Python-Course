d67b7e8e06a50b7e63650f36f38a35d8967874a1
Reza Akbari Movahed <32155999+r-akbarimovahed@users.noreply.github.com>
2022-08-23 09:29:01 +0430

    Add files via upload

---
+++ 6th session/Mycode2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  1 10:52:34 2022
+
+@author: Reza
+"""
+
+# x = -1 
+
+# if x<0: 
+#     raise Exception("Sorry, no numbers below zero")
+    
+# x = [12,5+9j,'E',"Python",False]
+
+# if len(x)<7: 
+#     raise Exception("Sorry, the number of elements of the list is less than 7")
+
+x = "hello"
+
+if type(x)!=int:
+    raise TypeError("Only integers are allowed")
+    
+
